0764e0f274931049533f39c4450e8e5228919c93
tvmir <tf2008@hw.ac.uk>
2024-03-16 12:34:13 +0400

    Implemented search, bar chart, and pie chart

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,74 @@
 'use strict';
 
+import BarChart from './barChart.js';
+import PieChart from './pieChart.js';
+
 console.log(`D3 loaded, version ${d3.version}`);
+
+// Loading data
+const loadData = async () => {
+  try {
+    let data = await d3.csv(
+      'data/Electric_Vehicle_Population_Data_Cleaned.csv'
+    );
+
+    return data;
+  } catch (error) {
+    console.error('Error loading data:', error);
+    throw error;
+  }
+};
+
+// Charts
+const barChart = new BarChart('#bar-chart');
+const pieChart = new PieChart('#pie-chart');
+
+// Helper function to filter data based on the car manufacturer ("Make" column)
+const filterCarMake = (data, make) => {
+  const filteredData = data.filter((d) =>
+    d.Make.toLowerCase().includes(make.toLowerCase())
+  );
+
+  // Storing each car model to the set
+  const modelSet = new Set();
+  filteredData.forEach((d) => modelSet.add(d.Model));
+
+  // Getting the total count of each car model
+  const modelCounts = Array.from(modelSet).map((model) => ({
+    model,
+    count: filteredData.filter((d) => d.Model === model).length,
+  }));
+
+  return modelCounts;
+};
+
+const handleSearch = (event) => {
+  // Search via "Enter"
+  if (event.keyCode === 13) {
+    const searchInput = document.querySelector('.search input').value.trim();
+
+    loadData().then((data) => {
+      // If search input is empty, render bar chart with car makes only
+      if (searchInput === '' || searchInput.length === 0) {
+        barChart.renderBarChart(data);
+        pieChart.renderPieChart(data);
+        // Otherwise, filter the data based on the search input
+      } else {
+        const filteredData = filterCarMake(data, searchInput);
+        barChart.renderBarChart(filteredData, true);
+        pieChart.renderPieChart(data, searchInput);
+      }
+    });
+  }
+};
+
+// Event listener to search input
+document
+  .querySelector('.search input')
+  .addEventListener('keydown', handleSearch);
+
+// Loading the defaulted data
+loadData().then((data) => {
+  barChart.renderBarChart(data);
+  pieChart.renderPieChart(data);
+});
---
+++ scripts/pieChart.js
@@ -0,0 +1,113 @@
+export default class PieChart {
+  constructor(svgSelector) {
+    this.svgSelector = svgSelector;
+  }
+
+  createPieChart(data) {
+    // Dimensions
+    const width = 200;
+    const height = 200;
+    const radius = Math.min(width, height) / 2;
+
+    // Reset the chart every time a search is being made
+    d3.select('#pie-chart').selectAll('*').remove();
+
+    // Assigning the colors to both types
+    const colors = d3
+      .scaleOrdinal()
+      .domain(data.map((d) => d.type))
+      .range(
+        data.map((d) => {
+          if (d.type === 'Plug-in Hybrid Electric Vehicle (PHEV)') {
+            return '#5E3FBE';
+          } else if (d.type === 'Battery Electric Vehicle (BEV)') {
+            return '#F4F0FD';
+          }
+        })
+      );
+
+    // Assigning the dimensions to the chart
+    const svg = d3
+      .select('#pie-chart')
+      .attr('width', width)
+      .attr('height', height);
+
+    // Adding the values
+    const pie = d3
+      .pie()
+      .value((d) => d.value)
+      .sort(null);
+
+    // Setting up the slices
+    const arc = d3.arc().innerRadius(0).outerRadius(radius);
+
+    const arcs = svg
+      .selectAll('.arc')
+      .data(pie(data))
+      .enter()
+      .append('g')
+      .attr('class', 'arc')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+    // Adding animations
+    arcs
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', (_, i) => colors(i))
+      .transition()
+      .duration(800)
+      .attrTween('d', (d) => {
+        const interpolate = d3.interpolate({ startAngle: 0, endAngle: 0 }, d);
+        return (t) => arc(interpolate(t));
+      });
+
+    // Showcasing the percentages in the chart
+    arcs
+      .append('text')
+      .attr('transform', (d) => `translate(${arc.centroid(d)})`)
+      .attr('dy', '0.35em')
+      .attr('text-anchor', 'middle')
+      .text((d) => `${d.data.value.toFixed(1)}%`)
+      .style('fill', (d) =>
+        d.data.type === 'Battery Electric Vehicle (BEV)' ? 'black' : 'white'
+      );
+
+    arcs.exit().remove();
+  }
+
+  renderPieChart(data, searchedMake) {
+    let filteredData;
+
+    // Checking if a car make is being searched
+    if (searchedMake !== undefined) {
+      filteredData = data.filter((d) =>
+        d.Make.toLowerCase().includes(searchedMake.toLowerCase())
+      );
+    } else {
+      filteredData = data;
+    }
+
+    // Extracting the Electric Vehicle Types from the data
+    const types = Array.from(
+      new Set(filteredData.map((d) => d['Electric Vehicle Type']))
+    );
+
+    // Calculating the counts for each Electric Vehicle Type
+    const typeCounts = types.map((type) => ({
+      type,
+      count: filteredData.filter((d) => d['Electric Vehicle Type'] === type)
+        .length,
+    }));
+
+    // Summing up the counts based on the type
+    const total = typeCounts.reduce((acc, curr) => acc + curr.count, 0);
+
+    // Calculating the percentages for each type
+    const percentages = typeCounts.map((type) => ({
+      type: type.type,
+      value: (type.count / total) * 100,
+    }));
+
+    this.createPieChart(percentages);
+  }
+}
