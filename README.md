e31683676c625d8d45250e7dc8f645cf272d9faa
tvmir <tf2008@hw.ac.uk>
2024-03-25 23:40:03 +0400

    Fixed css layout

--- README.md
+++ README.md
@@ -3,25 +3,27 @@
 This starter project contains a basic web application template for your group project.
 
 To do list:
- - [ ] One group member forks this project.
- - [ ] That same group member invites others to collaborate.
- - [ ] Each group member sets up their access to GitLab: SSH keygen; clone repo; configuration git user.email
+
+- [ ] One group member forks this project.
+- [ ] That same group member invites others to collaborate.
+- [ ] Each group member sets up their access to GitLab: SSH keygen; clone repo; configuration git user.email
 
 ## Tasks
 
 - [ ] Git Configuration
 - [ ] Basic HTML Structure
 - [ ] CSS Structure
-- [ ] Dashboard `<div>`
-  - [ ] CSS Grids
-  - [ ] Search Bar
+- [x] Dashboard `<div>`
+  - [x] CSS Grids
+  - [x] Search Bar/Functionality
 - [ ] Graphs
   - [ ] Bar Chart
-    - [ ] Total Number of Sold Cars - @tf2008
-      - [ ] Zoom Interactive functionality
+    - [x] Total Number of Sold Cars - @tf2008
+      - [x] Zoom Interactive functionality
     - [ ] Number of EVs Types sold by Year - @ae87
     - [ ] Distribution of Clean Alternative Fuel Vehicle by Model Year - @cr2007
-  - [ ] Pie Chart - @tf2008
+  - [x] Pie Chart
+    - [x] Proportion on Electric Vehicle Types - @tf2008
   - [ ] Line Chart
     - [ ] EVs Owned every year - @ae87
   - [ ] Geo Map Chart ([`d3.geo()`](https://d3js.org/d3-geo))
--- scripts/stackedBarChart.js
+++ scripts/stackedBarChart.js
@@ -6,7 +6,10 @@ export default class StackedBarChart {
 
   renderStackedBarChart(data) {
     // Correct keys based on your data structure
-    const keys = ['Battery Electric Vehicle (BEV)', 'Plug-in Hybrid Electric Vehicle (PHEV)'];
+    const keys = [
+      'Battery Electric Vehicle (BEV)',
+      'Plug-in Hybrid Electric Vehicle (PHEV)',
+    ];
 
     // Dimensions and margins
     const margin = { top: 20, right: 20, bottom: 40, left: 50 };
@@ -14,16 +17,19 @@ export default class StackedBarChart {
     const height = 300 - margin.top - margin.bottom;
 
     // Scales
-    const xScale = d3.scaleBand()
+    const xScale = d3
+      .scaleBand()
       .rangeRound([0, width])
       .padding(0.1)
-      .domain(data.map(d => d.year));
+      .domain(data.map((d) => d.year));
 
-    const yScale = d3.scaleLinear()
+    const yScale = d3
+      .scaleLinear()
       .rangeRound([height, 0])
-      .domain([0, d3.max(data, d => d[keys[0]] + d[keys[1]])]);
+      .domain([0, d3.max(data, (d) => d[keys[0]] + d[keys[1]])]);
 
-    const colorScale = d3.scaleOrdinal()
+    const colorScale = d3
+      .scaleOrdinal()
       .domain(keys)
       .range(['#f5a067', '#5E3FBE']); // Change these colors to match your desired scheme
 
@@ -31,22 +37,22 @@ export default class StackedBarChart {
     d3.select(this.svgSelector).selectAll('*').remove();
 
     // Append the SVG object to the body of the page
-    const svg = d3.select(this.svgSelector)
+    const svg = d3
+      .select(this.svgSelector)
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
     // Stack the data
-    const stackedData = d3.stack()
-      .keys(keys)(data);
+    const stackedData = d3.stack().keys(keys)(data);
 
     const yAxis = d3.axisLeft(yScale);
-    const yAxisGroup = svg.append('g')
-      .call(yAxis);
+    const yAxisGroup = svg.append('g').call(yAxis);
 
     // Add gridlines by cloning the tick lines and extending them across the chart
-    yAxisGroup.selectAll('.tick line')
+    yAxisGroup
+      .selectAll('.tick line')
       .clone()
       .attr('x2', width)
       .attr('stroke-opacity', 0.1) // Adjust the opacity as needed
@@ -54,38 +60,51 @@ export default class StackedBarChart {
       .lower();
 
     // Create the bars
-    svg.append('g')
+    svg
+      .append('g')
       .selectAll('g')
       .data(stackedData)
-      .enter().append('g')
-      .attr('fill', d => colorScale(d.key))
+      .enter()
+      .append('g')
+      .attr('fill', (d) => colorScale(d.key))
       .selectAll('rect')
-      .data(d => d)
-      .enter().append('rect')
-      .attr('x', d => xScale(d.data.year))
-      .attr('y', d => yScale(d[1]))
-      .attr('height', d => yScale(d[0]) - yScale(d[1]))
+      .data((d) => d)
+      .enter()
+      .append('rect')
+      .attr('x', (d) => xScale(d.data.year))
+      .attr('y', (d) => yScale(d[1]))
+      .attr('height', (d) => yScale(d[0]) - yScale(d[1]))
       .attr('width', xScale.bandwidth());
 
     // Add the X Axis
-    svg.append('g')
+    svg
+      .append('g')
       .attr('transform', `translate(0,${height})`)
       .call(d3.axisBottom(xScale));
 
     // Add the Y Axis
-    svg.append('g')
-      .call(d3.axisLeft(yScale));
-
+    svg.append('g').call(d3.axisLeft(yScale));
 
     // Add a tooltip
-    const tooltip = d3.select('body').append('div')
+    const tooltip = d3
+      .select('body')
+      .append('div')
       .attr('class', 'tooltip')
-      .style('opacity', 0);
+      .style('opacity', 0)
+      .style('position', 'absolute')
+      .style('padding', '5px')
+      .style('background-color', '#ffffff')
+      .style('border', '1px solid #cccccc')
+      .style('border-radius', '5px')
+      .style('box-shadow', '0 2px 2px rgba(0, 0, 0, 0.1)')
+      .style('pointer-events', 'none');
 
     // Tooltip mouseover event handler
     const mouseover = (event, d) => {
       tooltip.transition().duration(200).style('opacity', 0.9);
-      tooltip.html(`${d.key}<br/>${d.data.year}: ${d[1] - d[0]}`)
+      tooltip
+        // TODO: Name not consistent
+        .html(`${d[0] ? keys[0] : keys[1]}<br/>${d.data.year}: ${d[1] - d[0]}`)
         .style('left', `${event.pageX}px`)
         .style('top', `${event.pageY - 28}px`);
     };
@@ -96,9 +115,7 @@ export default class StackedBarChart {
     };
 
     // Add event listeners for the tooltip
-    svg.selectAll('rect')
-      .on('mouseover', mouseover)
-      .on('mouseout', mouseout);
+    svg.selectAll('rect').on('mouseover', mouseover).on('mouseout', mouseout);
 
     // Optionally, you can add zoom behavior here
     // ...
