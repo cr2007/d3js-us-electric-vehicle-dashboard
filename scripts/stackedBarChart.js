0494a9456cc404ab5d18a19f01b018a4854eedb4
Ahmed ElZubair <ae87@hw.ac.uk>
2024-04-05 01:23:05 +0400

    commented my code

--- scripts/stackedBarChart.js
+++ scripts/stackedBarChart.js
@@ -1,28 +1,43 @@
+// Import helper functions from helper.js
 import { mouseoverHandler, mouseoutHandler, axisLabel } from './helper.js';
 
+/**
+ * Class representing a StackedBarChart.
+ */
 export default class StackedBarChart {
+  /**
+   * Create a StackedBarChart.
+   * @param {string} svgSelector - The selector for the SVG element.
+   */
   constructor(svgSelector) {
+    // Store the SVG selector for later use
     this.svgSelector = svgSelector;
   }
 
+  /**
+   * Render the stacked bar chart.
+   * @param {Array} data - The data to render.
+   */
   renderStackedBarChart(data) {
+    // Log the data for debugging
     console.log('Stacked bar chart data:', data);
 
-    // Correct keys based on your data structure
+    // Define the keys for the data
     const keys = [
       'Battery Electric Vehicle (BEV)',
       'Plug-in Hybrid Electric Vehicle (PHEV)',
     ];
 
+    // Get the width of the container
     const container = d3.select('#stacked-bar-chart-container');
     const containerWidth = container.node().getBoundingClientRect().width;
 
-    // Dimensions and margins
+    // Define the margins, width, and height
     const margin = { top: 20, right: 20, bottom: 40, left: 80 };
     const width = containerWidth - 5;
     const height = 580 - margin.top - margin.bottom;
 
-    // Scales
+    // Define the scales
     const xScale = d3
       .scaleBand()
       .rangeRound([0, width])
@@ -34,15 +49,19 @@ export default class StackedBarChart {
       .rangeRound([height, 0])
       .domain([0, d3.max(data, (d) => d[keys[0]] + d[keys[1]])]);
 
+    // Define the y-axis
     const yAxis = d3.axisLeft(yScale);
 
+    // Define the color scale
     const colorScale = d3
       .scaleOrdinal()
       .domain(keys)
       .range(['#f5a067', '#5E3FBE']);
 
+    // Remove all elements from the SVG
     d3.select(this.svgSelector).selectAll('*').remove();
 
+    // Create the SVG element
     const svg = d3
       .select(this.svgSelector)
       .attr('width', '100%')
@@ -80,15 +99,16 @@ export default class StackedBarChart {
       .attr('y', (d) => yScale(d[1]))
       .attr('height', (d) => yScale(d[0]) - yScale(d[1]));
 
-    // Applying the x and y axis
+    // Add the x-axis
     const xAxisGroup = svg
       .append('g')
       .attr('transform', `translate(0,${height})`)
       .call(d3.axisBottom(xScale));
 
+    // Remove the domain line of the x-axis
     xAxisGroup.select('.domain').attr('display', 'none');
 
-    // Adding the X label
+    // Add the x-axis label
     axisLabel({
       axisGroup: xAxisGroup,
       orientation: 'x',
@@ -97,10 +117,13 @@ export default class StackedBarChart {
       text: 'Years',
     });
 
+    // Add the y-axis
     const yAxisGroup = svg.append('g').call(yAxis);
+
+    // Remove the domain line of the y-axis
     yAxisGroup.select('.domain').attr('display', 'none');
 
-    // Adding the Y label
+    // Add the y-axis label
     axisLabel({
       axisGroup: yAxisGroup,
       orientation: 'y',
@@ -109,7 +132,7 @@ export default class StackedBarChart {
       text: 'Number of Electric Vehicle Types Sold',
     });
 
-    // Add a tooltip
+    // Create the tooltip
     const tooltip = d3
       .select('body')
       .append('div')
@@ -123,9 +146,11 @@ export default class StackedBarChart {
       .style('box-shadow', '0 2px 2px rgba(0, 0, 0, 0.1)')
       .style('pointer-events', 'none');
 
+    // Define the content of the tooltip
     const content = (d) =>
       `${d[0] ? keys[1] : keys[0]}<br/>${d.data.year}: ${d[1] - d[0]}`;
 
+    // Add mouseover and mouseout events to the bars
     svg
       .selectAll('rect')
       .on('mouseover', mouseoverHandler(tooltip, content, 0.85))
