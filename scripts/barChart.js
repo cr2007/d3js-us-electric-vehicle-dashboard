12ff0b1cf28d1bcd24e4d183e2667c97ddd7b4f2
tvmir <tf2008@hw.ac.uk>
2024-04-04 13:17:21 +0400

    Code refactor and better documentation

--- scripts/barChart.js
+++ scripts/barChart.js
@@ -9,7 +9,7 @@ export default class BarChart {
     this.svgSelector = svgSelector;
   }
 
-  // Helper function to filter data based on the car manufacturer ("Make" column)
+  // Filters data by car manufacturer ("Make") to display its associated models
   filterCarModel(data, make) {
     const filteredData = data.filter((d) =>
       d.Make.toLowerCase().includes(make.toLowerCase())
@@ -26,31 +26,9 @@ export default class BarChart {
     return modelCounts;
   }
 
-  // Showcasing the bar chart
-  renderBarChart(data, displayModels = false) {
-    let counts = [];
-
-    if (displayModels) {
-      counts = data;
-    } else {
-      // Fetching all the car makes
-      const checkedMakes = getCheckedCarMakes(data, 'bc-dropdown-content');
-
-      const filteredData = data.filter((d) => checkedMakes.includes(d.Make));
-
-      const carMakes = d3.rollup(
-        filteredData,
-        (v) => v.length,
-        (d) => d.Make
-      );
-
-      counts = Array.from(carMakes, ([make, count]) => ({
-        make,
-        count,
-      }));
-    }
-
-    // Dynamic width
+  // Initializes the SVG based on the specified dimensions
+  createBarChart() {
+    // Dynamic width to fit the size of the container
     const container = d3.select('#bar-chart-container');
     const containerWidth = container.node().getBoundingClientRect().width;
 
@@ -61,34 +39,65 @@ export default class BarChart {
     const barChartWidth = width - margin.left - margin.right;
     const barChartHeight = height - margin.top - margin.bottom;
 
-    // Reset the chart every time a search is being made
-    d3.select('#bar-chart').selectAll('*').remove();
+    // Resetting the chart on each re-render
+    d3.select(this.svgSelector).selectAll('*').remove();
 
-    // Applying the default dimensions and zoom to the SVG
+    // Applying the default dimensions to the SVG container with dynamic width and fixed height
     const svg = d3
       .select(this.svgSelector)
-      .attr('viewBox', `0 0 ${containerWidth} ${height + 50}`)
+      .attr('viewBox', `0 0 ${width} ${height + 50}`)
       .attr('preserveAspectRatio', 'xMinYMin meet')
       .attr('width', '100%')
-      .style('display', 'block')
-      .call(zoom);
+      .style('display', 'block');
 
-    // Styling
+    // Initializing the margins and styling
     const barChart = svg
       .append('g')
       .attr('fill', '#5E3FBE')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    // Determining which property to use for the x-axis
-    const property = displayModels ? 'model' : 'make';
+    return { svg, barChart, barChartWidth, barChartHeight, margin };
+  }
 
-    // Setting up x and y scales
+  // Renders the bar chart to display the car "Make" or "Model" counts based on the user search
+  renderBarChart(data, isDisplayingModels = false) {
+    const { svg, barChart, barChartWidth, barChartHeight, margin } =
+      this.createBarChart();
+
+    let counts = [];
+
+    if (isDisplayingModels) {
+      counts = data;
+    } else {
+      // Fetching all the car makes and setting them as checked by default
+      const checkedMakes = getCheckedCarMakes(data, 'bc-dropdown-content');
+
+      const filteredData = data.filter((d) => checkedMakes.includes(d.Make));
+
+      const carMakes = d3.rollup(
+        filteredData,
+        (v) => v.length,
+        (d) => d.Make
+      );
+
+      // Structuring the values to to associate each car make with its total count
+      counts = Array.from(carMakes, ([make, count]) => ({
+        make,
+        count,
+      }));
+    }
+
+    // Interchanging between the "Make" and "Model" columns to be used on the same chart
+    const colData = isDisplayingModels ? 'model' : 'make';
+
+    // Setting up x-axis and its scale
     const xScale = d3
       .scaleBand()
-      .domain(counts.map((d) => d[property]))
+      .domain(counts.map((d) => d[colData]))
       .range([0, barChartWidth])
       .padding(0.15);
 
+    // X-axis to show each car "Make"
     const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
 
     const xAxisGroup = barChart
@@ -97,9 +106,10 @@ export default class BarChart {
       .attr('transform', `translate(0,${barChartHeight})`)
       .call(xAxis);
 
-    // Removing the defaulted lines for a better visual
+    // Removing the defaulted x-axis line for a better visual
     xAxisGroup.select('.domain').attr('display', 'none');
 
+    // Keeping the length of the x-axis values consistent to avoid overflowing
     xAxisGroup
       .selectAll('text')
       .attr('dy', '1em')
@@ -107,6 +117,7 @@ export default class BarChart {
       .style('font-size', '10px')
       .text((d) => (d.length > 6 ? d.substring(0, 5) + '...' : d));
 
+    // Adding the X label
     xAxisGroup
       .append('text')
       .attr('class', 'axis-label')
@@ -115,20 +126,23 @@ export default class BarChart {
       .style('font-size', '14px')
       .text('Car Makes');
 
+    // Setting up y-axis and its scale
     const yScale = d3
       .scaleLinear()
       .domain([0, d3.max(counts, (d) => d.count)])
       .nice()
       .range([barChartHeight, 0]);
 
-    // Y-axis counts
+    // Y-axis counts for the number of sold cars
     const yAxisGrp = barChart
       .append('g')
       .attr('class', 'y-axis')
       .call(d3.axisLeft(yScale));
 
+    // Removing the defaulted y-axis line for a better visual
     yAxisGrp.select('.domain').attr('display', 'none');
 
+    // Adding the Y label
     yAxisGrp
       .append('text')
       .attr('class', 'axis-label')
@@ -140,6 +154,7 @@ export default class BarChart {
       .style('font-size', '14px')
       .text('# of sold cars');
 
+    // Styling the tooltip on hover
     const tooltip = d3
       .select('body')
       .append('div')
@@ -153,17 +168,17 @@ export default class BarChart {
       .style('box-shadow', '0 2px 2px rgba(0, 0, 0, 0.1)')
       .style('pointer-events', 'none');
 
-    // What is displayed when hovering over a specific bar
-    const content = (d) => `${d[property]}<br/>${d.count}`;
+    // The content that is displayed when hovering over a specific bar
+    const content = (d) => `${d[colData]}<br/>${d.count}`;
 
-    // Adding the bars to the barChart
+    // Adding each bars to the barChart to fit within the SVG dimensions
     barChart
       .selectAll('.bar')
       .data(counts)
       .enter()
       .append('rect')
       .attr('class', 'bar')
-      .attr('x', (d) => xScale(d[property]))
+      .attr('x', (d) => xScale(d[colData]))
       .attr('y', barChartHeight)
       .attr('width', xScale.bandwidth())
       .attr('height', 0)
@@ -172,13 +187,13 @@ export default class BarChart {
       .transition()
       .duration(800)
       .attr('y', (d) => yScale(d.count))
-      .attr('height', (d) => barChartHeight - yScale(d.count));
+      .attr('height', (d) => barChartHeight - yScale(d.count)); // Animating the bars from bottom to top
 
-    // Responsible for applying zoom to the chart
+    // Setting up the zoom function on the SVG to fit within the chart
     function zoom(svg) {
       const extent = [
         [margin.left, margin.top],
-        [barChartWidth, height - margin.top],
+        [barChartWidth, barChartHeight],
       ];
 
       svg.call(
@@ -194,7 +209,7 @@ export default class BarChart {
 
             svg
               .selectAll('.bar')
-              .attr('x', (d) => xScale(displayModels ? d.model : d.make))
+              .attr('x', (d) => xScale(isDisplayingModels ? d.model : d.make))
               .attr('width', xScale.bandwidth())
               .attr('y', (d) => yScale(d.count))
               .attr('height', (d) => barChartHeight - yScale(d.count));
@@ -207,5 +222,8 @@ export default class BarChart {
           })
       );
     }
+
+    // Using the helper function above to apply the zoom behaviour on the SVG
+    svg.call(zoom);
   }
 }
